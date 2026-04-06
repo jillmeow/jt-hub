b9cf3fd1906d124421c29de493cd129cbd7a62ee
Jill van der Veen <jillmirandilla25@gmail.com>
2026-04-06 17:40:09 +1200

    To use react version of the Justin Timberlake app

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+const Greetings = () => <h1>Welcome Justin Timberlake</h1>
+
+const root = createRoot(document.getElementById('target'))
+
+root.render (
+    <Greetings />
+)
+/*import _ from 'lodash';
+
+function component () {
+    const element = document.createElement('div');
+    element.innerHTML = _.join(["Hello", "Justin Timberlake"], " ");
+
+    return element;
+}
+
+document.body.appendChild(component());*/
+
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    }
+};
